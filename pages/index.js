f38c400a547d9cbf3ad34f9e8442e8c8dd38b232
Mario Souto <soutomarios@gmail.com>
2021-02-08 19:26:43 -0300

    Modulo 01 Aula 07

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Footer from '../src/components/commons/Footer'
 import Menu from '../src/components/commons/Menu'
+import { Text } from '../src/components/foundation/Text'
+import { Button } from '../src/components/commons/Button'
 
 export default function Home() {
   return (
@@ -11,6 +13,46 @@ export default function Home() {
       justifyContent: 'space-between',
     }}>
       <Menu />
+
+
+      <div>
+        <Text
+          variant="title"
+          tag="h1"
+          color="tertiary.main"
+          textAlign={{
+            xs: 'center',
+            md: 'left',
+          }}
+        >
+          Compartilhe momentos e conecte-se com amigos
+        </Text>
+        <Text
+          variant="paragraph1"
+          tag="p"
+          color="tertiary.light"
+          textAlign={{
+            xs: 'center',
+            md: 'left',
+          }}
+        >
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
+        </Text>
+
+        <Button
+          variant="primary.main"
+          margin={{
+            xs: 'auto',
+            md: 'initial',
+          }}
+          display="block"
+        >
+          Cadastrar
+        </Button>
+      </div>
+
+
       <Footer />
     </div>
   )
