881899b5ffab963b1f778cde3434cf5d8e80dc33
Mario Souto <soutomarios@gmail.com>
2021-02-08 19:22:43 -0300

    Modulo 01 Aula 05

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Logo } from '../../../theme/Logo';
+import { Text } from '../../foundation/Text';
 import { Button } from '../Button';
 import { MenuWrapper } from './styles/MenuWrapper';
 
@@ -17,9 +18,9 @@ export default function Menu() {
           { url: '/sobre', name: 'Sobre' },
         ].map((link) => (
           <li key={link.url}>
-            <a href={link.url}>
+            <Text variant="smallestException" tag="a" href={link.url}>
               {link.name}
-            </a>
+            </Text>
           </li>
         ))}
       </MenuWrapper.CentralSide>
---
+++ src/theme/GlobalStyle/index.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  ${normalize}
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: ${({ theme }) => theme.fontFamily};
+  }
+
+  /* Full height layout */
+  html, body {
+    display: flex;
+    min-height: 100vh;
+    width: 100%;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export default GlobalStyle;
