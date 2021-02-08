d7e486315bef46cb8f2d8664aaf12c085ca77804
Mario Souto <soutomarios@gmail.com>
2021-02-08 19:28:10 -0300

    Modulo 01 Aula 08

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Footer from '../src/components/commons/Footer'
 import Menu from '../src/components/commons/Menu'
 import { Text } from '../src/components/foundation/Text'
 import { Button } from '../src/components/commons/Button'
+import { Grid } from '../src/components/layout/Grid'
 
 export default function Home() {
   return (
@@ -14,45 +15,59 @@ export default function Home() {
     }}>
       <Menu />
 
+      <Grid.Container>
+        <Grid.Row>
+          <Grid.Col
+            offset={1}
+            value={{ xs: 12, md: 5 }}
+          >
+            <div>
+              <Text
+                variant="title"
+                tag="h1"
+                color="tertiary.main"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Compartilhe momentos e conecte-se com amigos
+              </Text>
+              <Text
+                variant="paragraph1"
+                tag="p"
+                color="tertiary.light"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
+              </Text>
 
-      <div>
-        <Text
-          variant="title"
-          tag="h1"
-          color="tertiary.main"
-          textAlign={{
-            xs: 'center',
-            md: 'left',
-          }}
-        >
-          Compartilhe momentos e conecte-se com amigos
-        </Text>
-        <Text
-          variant="paragraph1"
-          tag="p"
-          color="tertiary.light"
-          textAlign={{
-            xs: 'center',
-            md: 'left',
-          }}
-        >
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
-        </Text>
-
-        <Button
-          variant="primary.main"
-          margin={{
-            xs: 'auto',
-            md: 'initial',
-          }}
-          display="block"
-        >
-          Cadastrar
-        </Button>
-      </div>
-
-
+              <Button
+                variant="primary.main"
+                margin={{
+                  xs: 'auto',
+                  md: 'initial',
+                }}
+                display="block"
+              >
+                Cadastrar
+              </Button>
+            </div>
+            </Grid.Col>
+          <Grid.Col
+            value={{ xs: 12, md: 6 }}
+          >
+            <img
+              style={{ display: 'block', margin: 'auto' }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
       <Footer />
     </div>
   )
