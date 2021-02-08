9b406aa0bd12a853e73ffeb4d2b5bb6a1688bfd7
Mario Souto <soutomarios@gmail.com>
2021-02-08 19:20:31 -0300

    Modulo 01 Aula 04

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,20 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components'
 import theme from '../src/theme';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
+import GlobalStyle from '../src/theme/GlobalStyle';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
+import Footer from '../src/components/commons/Footer'
 import Menu from '../src/components/commons/Menu'
-
-// import styled from 'styled-components'
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
 
 export default function Home() {
   return (
-    <div>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet"
-      />
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}>
       <Menu />
+      <Footer />
     </div>
   )
 }
