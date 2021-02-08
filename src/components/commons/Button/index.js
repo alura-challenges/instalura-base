40acfcdc556ff00bd7d9a637ec4ca6ec5801d8b1
Mario Souto <soutomarios@gmail.com>
2021-02-08 19:17:37 -0300

    Modulo 01 Aula 03

---
+++ src/components/commons/Button/index.js
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+import get from 'lodash/get';
+
+const ButtonGhost = css`
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+  background-color: transparent;
+`;
+
+const ButtonDefault = css`
+  color: ${({ theme, variant }) => get(theme, `colors.${variant}.contrastText`)};
+  background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+`;
+
+export const Button = styled.button`
+  border: 0;
+  cursor: pointer;
+  padding: 12px 26px;
+  font-weight: bold;
+  opacity: 1;
+  transition: opacity ${({ theme }) => theme.transition};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
+  &:hover,
+  &:focus {
+    opacity: .5;
+  }
+`;
