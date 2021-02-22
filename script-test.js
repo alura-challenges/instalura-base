ca6530d40eb00204d272cec9f4a2eb684c2c0f8a
Mario Souto <soutomarios@gmail.com>
2021-02-22 16:39:59 -0300

    final m2a5

---
+++ script-test.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const shell = require('shelljs');
+
+console.log('Ola mundo');
+
+const resultado = shell.exec('git diff --name-only branch-scripts-testes..main', { silent: true })
+
+console.log(resultado.stdout.split('\n'));
