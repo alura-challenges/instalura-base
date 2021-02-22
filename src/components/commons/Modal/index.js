0667f4cb6d23372fe5c5212eb298bd89bc15b723
Mario Souto <soutomarios@gmail.com>
2021-02-22 16:02:58 -0300

    final aula 01

---
+++ src/components/commons/Modal/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  background: rgba(0,0,0,0.1);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  overflow: scroll;
+
+  ${({ isOpen }) => {
+    if (isOpen) {
+      return css`
+        opacity: 1;
+        pointer-events: all;
+      `;
+    }
+    return css`
+      opacity: 0;
+      pointer-events: none;
+    `;
+  }}
+`;
+
+function Modal({ isOpen, onClose, children }) {
+  return (
+    <ModalWrapper
+      isOpen={isOpen}
+      onClick={(event) => {
+        const isSafeArea = event.target.closest('[data-modal-safe-area="true"]');
+        // isOpen = false;
+        if (!isSafeArea) {
+          onClose();
+        }
+      }}
+    >
+      {children({
+        'data-modal-safe-area': 'true',
+      })}
+    </ModalWrapper>
+  );
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
