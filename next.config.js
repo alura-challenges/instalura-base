7f50e0335a8b1e6e1a2cbc83cb6a8830f89c4274
Mario Souto <soutomarios@gmail.com>
2021-03-07 13:49:31 -0300

    feat: update aula 08 fim

--- next.config.js
+++ next.config.js
@@ -5,4 +5,17 @@ module.exports = {
   async redirects() {
     return redirects;
   },
+  async headers() {
+    return [
+      {
+        source: '/app/:path*/',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
 };
