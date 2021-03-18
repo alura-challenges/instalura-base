80e43e8a21b8425c97d970943465a201320626dc
Mario Souto <soutomarios@gmail.com>
2021-03-18 13:50:41 -0300

    feat: final m4a2p2

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -3,6 +3,9 @@
 describe('/pages/app/login/', () => {
   // it === test que estamos fazendo
   it('preencha os campos e vá para a página /app/profile', () => {
+    cy.intercept('https://instalura-api.omariosouto.vercel.app/api/login')
+      .as('userLogin');
+
     cy.visit('/app/login/');
 
     // preencher o input usuario
@@ -17,5 +20,17 @@ describe('/pages/app/login/', () => {
 
     // o que esperamos? ir para "/app/profile/"
     cy.url().should('include', '/app/profile');
+
+    // Temos o token?
+    cy.wait('@userLogin')
+      .then((intercept) => {
+        // token do servidor
+        const { token } = intercept.response.body.data;
+
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          // token do cookie é igual ao do server?
+          .should('have.property', 'value', token);
+      });
   });
 });
