6b4531abf66aaaa90efcb6a0bc98abcb180fdded
Mario Souto <soutomarios@gmail.com>
2021-03-18 14:13:49 -0300

    feat: final m4a8

--- src/infra/hooks/forms/useForm/index.js
+++ src/infra/hooks/forms/useForm/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+function formatErrors(yupErrorsInner = []) {
+  return yupErrorsInner.reduce((errorObjectAcc, currentError) => {
+    const fieldName = currentError.path;
+    const errorMessage = currentError.message;
+    return {
+      ...errorObjectAcc,
+      [fieldName]: errorMessage,
+    };
+  }, {});
+}
+
 export function useForm({
   initialValues,
   onSubmit,
@@ -17,21 +28,17 @@ export function useForm({
       setErrors({});
       setIsFormDisabled(false);
     } catch (err) {
-      const formatedErrors = err.inner.reduce((errorObjectAcc, currentError) => {
-        const fieldName = currentError.path;
-        const errorMessage = currentError.message;
-        return {
-          ...errorObjectAcc,
-          [fieldName]: errorMessage,
-        };
-      }, {});
+      const formatedErrors = formatErrors(err.inner);
       setErrors(formatedErrors);
       setIsFormDisabled(true);
     }
   }
 
   React.useEffect(() => {
-    validateValues(values);
+    validateValues(values)
+      .catch((err) => {
+        console.log(err);
+      });
   }, [values]);
 
   return {
