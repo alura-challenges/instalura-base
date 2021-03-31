b3d9c687be662b9373bc336e94fe62c1b48e22ef
Mario Souto <soutomarios@gmail.com>
2021-03-31 16:58:10 -0300

    feat: fim m5a2

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import AboutScreen from '../src/components/screens/AboutScreen';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function PageSobre() {
-  return (
-    <div>
-      Página Sobre
-    </div>
-  );
+export async function getStaticProps() {
+  const TOKEN = '0aebf8f7bcffee44fbe032cf0e9cde';
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      pageSobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen);
