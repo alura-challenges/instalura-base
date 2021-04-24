45fece9e697417d5cbe416cae7a219b07a7eab53
Bruno Noriller <brunonoriller@gmail.com>
2021-04-24 15:35:03 -0300

    feat: :sparkles: mobile fix

--- src/components/wrappers/WebsitePage/hoc/MobilePadding.js
+++ src/components/wrappers/WebsitePage/hoc/MobilePadding.js
@@ -3,7 +3,7 @@ import { breakpointsMedia } from '../../../../theme/utils/breakpointsMedia';
 
 export const MobilePadding = styled.div`
 ${breakpointsMedia({
-  xs: css`padding-bottom: 75px;`,
-  md: css`padding-bottom: 0;`,
-})}
+    xs: css`padding-bottom: 75px;`,
+    md: css`padding-bottom: 0;`,
+  })}
 `;
